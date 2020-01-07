a2f78910716d4e2c8f7f19c1d78e4a1162ac44a7
Romeo <rommar@jpj.com>
2020-01-07 20:46:25 +0800

    Added sample usage of @Before and @After

--- src/test/java/com/examples/MultipleOrContainsCheckerTest.java
+++ src/test/java/com/examples/MultipleOrContainsCheckerTest.java
@@ -1,19 +1,26 @@
 package com.examples;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MultipleOrContainsCheckerTest {
 
+    private MultipleOrContainsChecker multipleOrContainsChecker;
+
+    @Before
+    public void beforeEach() {
+        multipleOrContainsChecker = new MultipleOrContainsChecker(5);
+    }
+
     @Test
     public void testShouldReturnTrueWhenValueIsMultipleOfN() {
-        MultipleOrContainsChecker multipleOrContainsChecker = new MultipleOrContainsChecker(5);
         Assert.assertTrue(multipleOrContainsChecker.isMultipleOrContains(20));
     }
 
     @Test
     public void testShouldReturnFalseWhenValueIsNotMultipleOfN() {
-        MultipleOrContainsChecker multipleOrContainsChecker = new MultipleOrContainsChecker(5);
         Assert.assertFalse(multipleOrContainsChecker.isMultipleOrContains(26));
     }
 
@@ -21,6 +28,11 @@ public class MultipleOrContainsCheckerTest {
     public void testShouldReturnTrueWhenValueContainsN() {
         MultipleOrContainsChecker multipleOrContainsChecker = new MultipleOrContainsChecker(4);
         Assert.assertTrue(multipleOrContainsChecker.isMultipleOrContains(34));
+    }
+
+    @After
+    public void afterEach() {
+        System.out.println("Test Finished!");
     }
 
 }
